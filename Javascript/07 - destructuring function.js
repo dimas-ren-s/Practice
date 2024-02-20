bd859f1e0e378274cfb7af349aeb44eafc861562
Kelompok Gatau <dimassugarareal@gmail.com>
2024-02-20 17:26:19 +0700

    destructuring(functionJs): membuat contoh destructuring dengan function dari return dan param

---
+++ Javascript/07 - destructuring function.js
@@ -0,0 +1,42 @@
+// Destructuring function adalah memanfaatkan destructuring array dan object didalam parameter atau return pada function
+// function return berupa array :
+function calculate(a, b) {
+    return [a + b, a - b, a * b, a / b]; // pakai [ ] agar return dalam bentuk array
+}
+
+// deklarasi destructuring array dari sebuah function dengan return array yang tersusun rapi
+const [tambah, kurang, kali, bagi] = calculate(2, 4);
+console.log(tambah, kurang, kali, bagi);
+
+// function return berupa object :
+function calculateObj(a, b) {
+    return {
+        add: a + b,
+        less: a - b,
+        multiply: a * b,
+        divide: a / b
+    }; // pakai { } dan key properti agar return dalam bentuk object
+}
+
+// deklarasi destructuring object tanpa perlu mengatur deret, karena ada key propertinya
+const { add, less, multiply, divide, modulus = "Tidak ada" } = calculateObj(2, 4);
+console.log(add, less, multiply, divide, modulus);
+
+// function dimana parameter berupa object
+const mahasiswa = {
+    id: 1908107010040,
+    nama: "dimas rendy",
+    nilai: {
+        uts: 90,
+        uas: 67,
+        latihan: 95
+    }
+}
+
+function cekNilai({ id, nama, nilai: { uts, uas, latihan } }) { // jangan lupa { }, kalau ada object didalam object, pecah dengan : { childObjectProperti, ... }
+    return `
+        Mahasiswa dengan NIM ${id} Nama ${nama}, memiliki nilai UAS ${uas}
+    `
+}
+
+console.log(cekNilai(mahasiswa));
